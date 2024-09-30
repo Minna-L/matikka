ff0842af12cf22879b6eaa69ff91f62297375e2a
Minna <minna.lumme@gmail.com>
2024-09-30 10:40:07 +0300

    matikka

---
+++ js funktions.js
@@ -0,0 +1,39 @@
+'user strict'
+
+let rand_num1 = 0;
+let rand_num2 = 0;
+let correctCount = 0;
+let wrongCount = 0;
+
+const getRandomIntNumberInRange = (min, max) => {
+    return Math.floor(Math.random() * max) + min;
+}
+
+const randomizeNumbers = () => {
+    rand_num1 = getRandomIntNumberInRange(1,10)
+    rand_num2 = getRandomIntNumberInRange (1,10)
+    document.querySelector('#num1').innerHTML = rand_num1
+    document.querySelector ('#num2').innerHTML = rand_num2
+}
+
+addEventListener("DOMContentLoaded", () => {
+    randomizeNumbers ()
+});
+document.querySelector('button').addEventListener('click', () => {
+    const answer = Number (document.querySelector('input').value)
+    const correctAnswer = rand_num1 + rand_num2
+    if (answer === correctAnswer) {
+        alert('Correct!');
+        correctCount ++;
+        document.querySelector('#correctCount').innerHTML = correctCount;
+    } 
+    else {
+        alert ('Inncorrect!')
+        wrongCount++;
+        document.querySelector('#wrongCount').innerHTML = wrongCount;
+    }
+    randomizeNumbers()
+    document.querySelector ('input').value = ''
+})
+
+
